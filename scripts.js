e6b468290b3a0eb9d7975a024669f24ec2d23256
Max <maximgrekhov009@gmail.com>
2026-08-19 14:34:30 +0500

    save3

--- scripts.js
+++ scripts.js
@@ -1,2 +1,43 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let passwordOutput1 = document.getElementById("output-one")
+let passwordOutput2 = document.getElementById("output-two")
+
+
+function getRandomPassword() {
+
+    passwordOutput1.textContent = ""
+    passwordOutput2.textContent = ""
+
+    for (let i = 0; i < 15; i++) {
+        let randomItem1 = Math.floor( Math.random() * characters.length )
+        passwordOutput1.textContent += characters[randomItem1]
+    }
+
+    for (let j = 0; j < 18; j++) {
+        let randomItem2 = Math.floor( Math.random() * characters.length )
+        passwordOutput2.textContent += characters[randomItem2]
+    }
+}
+
+function copyPassword(event) {
+
+    const passwordText = event.target.textContent;
+
+    if (!passwordText) {
+        alert("Сначала сгенерируйте пароль!");
+        return;
+    }
+
+    navigator.clipboard.writeText(passwordText)
+        .then(() => {
+            alert("Copied!");
+        })
+        .catch(err => {
+            console.error("Error: ", err);
+        });
+}
+
+passwordOutput1.addEventListener("click", copyPassword);
+passwordOutput2.addEventListener("click", copyPassword);
